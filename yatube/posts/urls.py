d60e59004361ddee4e14fead0ef907982a9f44c6
Стас Басов <stas.basov@whatever.com>
2023-02-11 14:52:22 +0300

    добавлены urls и view-функции

---
+++ yatube/posts/urls.py
@@ -0,0 +1,12 @@
+# posts/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index),
+    # Страница со списком постов пользователя
+    # path('posts/', views.posts_list),
+    # Отдельная страница с информацией о посте
+    path('group/<slug:slug>/', views.group_posts),
+]
